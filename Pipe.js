af36dc203fddd5f674fb803fa5b96a78275ba9e0
MuhiaKevin <archerkreiger@gmail.com>
2018-09-23 19:33:57 +0300

    fixed the two pipes

--- Pipe.js
+++ Pipe.js
@@ -1,35 +1,30 @@
-const Pipe = function(x, y,image, context){
-  this.x = x;
-  this.y = y;
+const Pipe = function(context,canvasWidth){
   this.context = context;
-  this.image = image;
+  this.canvasWidth = canvasWidth;
   this.velX = 4;
-  this.sprites = [document.getElementById('bottomPipe'),document.getElementById('topPipe')]
+  this.pipeBottom = document.getElementById('bottomPipe');
+  this.pipeTop = document.getElementById('topPipe');
+  this.pipe = [];
+  this.gap = 50;
+  this.pipe[0] = {
+    x : this.canvasWidth,
+    y : 0
+  }
+
 }
 
 
-
-
 Pipe.prototype.update = function(){
-  this.x -= this.velX;
+  this.pipe[0].x -= this.velX;
 
-      // Check if the pipe is out of the screen
-      if (this.x  <= 0) {
-          this.x = document.getElementById('screen').width; // Make it jump to the right side of the screen
-
-          // If the pipe is the top one
-          if (this.y <= 320) {
-              this.y = -(Math.random() * (150 - 50) + 50); // Math.random() * (max - min) + min
-              // If the pipe is the bottom one
-          } else {
-              this.y = 320 + (Math.random() * (150 - 50) + 50);
-          }
-      }
 }
 
 
 Pipe.prototype.render = function (){
+  for (var i = 0; i < this.pipe.length; i++) {
+    this.context.drawImage(this.pipeTop,this.pipe[i].x,this.pipe[i].y-300);
+    this.context.drawImage(this.pipeBottom,this.pipe[i].x,this.pipe[i].y+400);
+    // console.log(this.pipe[i].x)
 
-  this.context.drawImage(this.image,this.x,this.y);
-
+  }
 }
