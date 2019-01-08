5b5c50854d5253178ac7d29d6e5b3980ee99c5f3
Kevin Muhia <archerkreiger@gmail.com>
2019-01-08 14:41:42 +0300

    some cleaning

--- Enviroment.js
+++ Enviroment.js
@@ -22,9 +22,6 @@ Enviroment.prototype.update = function () {
 
 // draw function
 Enviroment.prototype.render = function () {
-
-  
-
   for (var i = 0; i <= this.canvas.width/this.bgImg.width+1; i++) {
     this.context.drawImage(this.bgImg,this.bgPos+i*this.bgImg.width,0);
 
